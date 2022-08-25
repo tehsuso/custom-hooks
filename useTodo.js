26c345381310c7173d723caf67a9dd28666a72e2
tehsuso <como_hithop_nada@hotmail.com>
2022-08-25 09:01:40 +0200

    First commit

---
+++ useTodo.js
@@ -0,0 +1,53 @@
+import { todoReducer } from "../08-useReducer/todoReducer"
+import { useEffect, useReducer } from "react"
+
+            
+const init = () => {
+    return JSON.parse( localStorage.getItem('todos')) || []
+}
+
+export const useTodo = ( initialState = [] ) => {
+
+    const [ todos, dispatch ] = useReducer ( todoReducer, initialState, init )
+    
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify( todos ))
+    }, [todos])
+    
+            
+    const handleNewTodo = ( todo ) => {
+        const action = {
+            type: 'Add Todo',
+            payload: todo
+        }
+        
+        dispatch( action )
+    }
+    
+    const handleDeleteTodo = ( id ) => {
+        dispatch({
+            type: 'Remove Todo',
+            payload: id
+        })   
+    }
+    
+    const handleToggleTodo = ( id ) => {
+        dispatch({
+            type: 'Toggle Todo',
+            payload: id,
+        })
+    }
+
+    const todosCount = todos.length
+
+    const PendingTodosCount = todos.filter(todo=> !todo.done).length
+
+    return {
+        todos,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+        todosCount,
+        PendingTodosCount,
+    }
+}
